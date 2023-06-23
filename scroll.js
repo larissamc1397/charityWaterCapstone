162e7c84911f7f124aff58a1121ed3f77d2d5c93
larissamc1397 <129820595+larissamc1397@users.noreply.github.com>
2023-06-22 19:19:47 -0500

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,6 +1,5 @@
-function scroll (){
-  const scrollButton = document.querySelector(".scrolltop");
-  scrollButton.addEventListener("click", () => {
-    window.scrollTo(0, 0);
-  });
-  }
+const scrollButton = document.querySelector(".scroll-top");
+scrollButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+}
+);
